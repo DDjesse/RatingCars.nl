5fdeaf08de0d9c36151db932afe7fce4da0a0d2e
DDs <114984073+DDjesse@users.noreply.github.com>
2024-11-20 14:12:20 +0100

    1.0

--- includes/db.php
+++ includes/db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";  // Gebruik je eigen database gebruikersnaam
 $password = "";      // Gebruik je eigen wachtwoord
-$dbname = "cars_db"; // De naam van je database
+$dbname = "Autoposts"; // De naam van je database
 
 // Maak de verbinding
 $conn = new mysqli($servername, $username, $password, $dbname);
